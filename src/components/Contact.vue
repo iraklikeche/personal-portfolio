3ce2e17a32b2c5c657cf73393ac7f8ee091b55f4
iraklikeche <irakli.kechee@gmail.com>
2023-10-21 15:11:12 +0400

    Implement email sending through emailjs

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -6,41 +6,62 @@
       </div>
       <div>
         <h2 class="mb-12 text-5xl font-bold">Get In Touch</h2>
-        <form>
+
+        <!-- <form ref="form" @submit.prevent="sendEmail">
+
+          <input type="text" name="user_name" />
+ 
+          <input type="email" name="user_email" />
+ 
+          <textarea name="message"></textarea>
+          <input type="submit" value="Send" />
+        </form> -->
+
+        <form ref="formData" @submit.prevent="sendEmail">
           <div class="flex gap-2 mb-2">
             <input
               type="text"
+              name="user_name"
               placeholder="First Name"
               class="input rounded-3xl px-6 py-5 w-full text-lg transition duration-1000 ease-in-out"
+              required
             />
             <input
               type="text"
               placeholder="Last Name"
+              name="user_lastName"
               class="input rounded-3xl px-6 py-5 w-full text-lg transition duration-1000 ease-in-out"
             />
           </div>
           <div class="flex gap-2 mb-2">
             <input
               type="email"
+              name="user_email"
               placeholder="Email"
               class="input rounded-3xl px-6 py-5 w-full text-lg transition duration-1000 ease-in-out"
+              required
             />
             <input
               type="tel"
               placeholder="Phone Number"
+              name="user_phone"
               class="input rounded-3xl px-6 py-5 w-full text-lg transition duration-1000 ease-in-out"
             />
           </div>
           <textarea
-            rows="6"
-            class="w-full transition duration-1000 ease-in-out"
+            rows="5"
+            name="message"
+            class="w-full px-6 py-2 text-lg rounded-3xl transition duration-1000 ease-in-out mb-6"
+            required
+            placeholder="Leave Your Message."
           />
 
           <button
             type="submit"
+            value="send"
             class="font-bold text-black bg-white py-4 px-12 relative transition duration-1000 ease-in-out hover:text-white"
           >
-            <span class="z-1 relative">
+            <span class="z-1 relative tracking-wider">
               {{ buttonText }}
             </span>
           </button>
@@ -51,20 +72,37 @@
 </template>
 
 <script setup>
+import emailjs from "@emailjs/browser";
 import { ref } from "vue";
 import contactImg from "../assets/img/contact-img.svg";
 
-const formInitialDetails = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  phone: "",
+const formData = ref({
+  user_name: "",
+  user_lastName: "",
+  user_email: "",
+  user_phone: "",
   message: "",
-};
+});
 
-const formDetails = ref(formInitialDetails);
-const buttonText = ref("Send");
-const status = ref({});
+const buttonText = ref("SEND");
+
+const sendEmail = () => {
+  emailjs
+    .sendForm(
+      "service_qnr4vjg",
+      "template_7hvu3ls",
+      formData.value, // $refs can be accessed directly in <script setup>
+      "9tBkyrPHTO01_gnU8"
+    )
+    .then(
+      (result) => {
+        console.log("SUCCESS!", result.text);
+      },
+      (error) => {
+        console.log("FAILED...", error.text);
+      }
+    );
+};
 </script>
 
 <style scoped>
